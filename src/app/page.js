c329b549fed95e6cfc65ecc6c13b8eb7b9bfdae3
JoelJanssson <joel.jansson@elev.ga.ntig.se>
2024-11-20 15:45:55 +0100

    Changed the homepage

--- src/app/page.js
+++ src/app/page.js
@@ -9,26 +9,26 @@ export default function Home() {
         </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          {/* <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href=""
             target="_blank"
             rel="noopener noreferrer"
           >
             Projects
-          </a>
+          </a> */}
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href=""
+            className="rounded-full bg-steel-blue-tint border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="/portfolio/CV"
             target="_blank"
             rel="noopener noreferrer"
           >
-            About me!
+            Resumé
           </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
+        {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href=""
           target="_blank"
@@ -36,13 +36,13 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/portfolio/file.svg"
+            src="/file.svg"
             alt="File icon"
             width={16}
             height={16}
           />
-          Resumé
-        </a>
+          About me!
+        </a> */}
       </footer>
     </div>
   );
