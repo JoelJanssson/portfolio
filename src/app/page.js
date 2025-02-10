dfd4332e8eb84e8e6ebcaffa22f1c33e53c62f97
JoelJanssson <joel.jansson@elev.ga.ntig.se>
2025-02-10 11:33:47 +0100

    Added button to project page

--- src/app/page.js
+++ src/app/page.js
@@ -9,18 +9,16 @@ export default function Home() {
         </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          {/* <a
+          <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href=""
-            target="_blank"
+            href="/portfolio/projects"
             rel="noopener noreferrer"
           >
             Projects
-          </a> */}
+          </a>
           <a
             className="rounded-full bg-steel-blue-tint border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="/portfolio/CV"
-            target="_blank"
             rel="noopener noreferrer"
           >
             Resumé
